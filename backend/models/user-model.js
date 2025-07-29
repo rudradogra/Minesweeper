25bf5c5e4999e76c2169d25c4035343cbb5c23c6
Rishi Durgapal <risg=hidurgapal@gmail.com>
2025-07-29 21:38:06 +0530

    sign in and sign up updated in user routes

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
+  email: {type:String, required: true, unique: true},
+  password: { type: String, required: true },
   highScore: { type: Number, default: 0 },
   gamesPlayed: { type: Number, default: 0 },
   totalTimePlayed: { type: Number, default: 0 }, // in seconds
